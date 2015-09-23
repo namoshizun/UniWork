864dab73b20c2a3816fa97658084cca70c87d71e
namoshizun <ptx1962@gmail.com>
2015-09-23 11:36:29 +1000

    Create Coordinate.java

---
+++ JAVA/Battleship/util/Coordinate.java
@@ -0,0 +1,23 @@
+package util;
+
+/**
+ * Do not alter this file in any way.
+ */
+
+public class Coordinate {
+	private int x;
+	private int y;
+	
+	public Coordinate(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+}
