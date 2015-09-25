b0f44acc6ada9e40bb8d8734e57c373518bf1687
namoshizun <ptx1962@gmail.com>
2015-09-25 21:21:09 +1000

    Update BalancedBinaryTree.java

--- JAVA/playWithAlgos/Dynamic_Programming/BalancedBinaryTree.java
+++ JAVA/playWithAlgos/Dynamic_Programming/BalancedBinaryTree.java
@@ -1,107 +1,68 @@
 package homework;
 
-import java.math.*;
 import java.util.*;
+import java.math.*;
 
-/**
- * - 
- * Description: A binary search tree is balanced if for every node in the graph, the
- * heights of the left and right subtrees differ by at most 1.
- * 
- * Let T(n) be the number of different balanced binary search trees on n keys.
- * Implement an algorithm that uses dynamic programming to compute T(n) for a
- * given input n.
- * 
- * Your algorithm should run in O(n^2 logn) time. Your program should take a
- * single command line argument (the number of keys) and print the number of
- * balanced binary trees.
- * 
- * Recurrence: T(n) =Sum{T(k-1)T(n-k)}, k = [lowerBound, upperBound] is the kth
- * key By finding the minimum size of the lft subtree, we can then determine the
- * range of 'k'.
- */
+public class bbst_sol {
 
-public class bbst {
+	/**
+	 * A binary search tree is balanced if for every node in the graph, the
+	 * heights of the left and right subtrees differ by at most 1. 
+	 * 
+	 * Let T(n) be the number of different balanced binary search trees on n keys
+	 * Calculate T(n)
+	 * */
+	public int getBbstNum(int nNodes) {
 
-	/*-
-	 * Constructor:
-	 * levelCapacity shows the max total number of nodes with i many levels. 
-	 * The first level is level 1
-	 */
-	List<Integer> levelCapacity;
-	BigInteger[] results;
+		// M[i,k] = # of BBST with i nodes and height k.
+		// More generally the size of M is nNodes + 1, but nNodes + ceiling avoids overflow when n < 10.
+		int ceiling = (int) Math.ceil(2 * (Math.log(nNodes + 1) / Math.log(2)) + 2);
+		int[][] M = new int[nNodes + ceiling][nNodes + ceiling];
+		int numOfBBST = 0;
 
-	public bbst(int bufferSize) {
-		levelCapacity = new ArrayList<Integer>();
-		results = new BigInteger[bufferSize]; // WORST DESIGN EVER :D
-		for (int i = 0; i < bufferSize; ++i)  results[i] = BigInteger.valueOf(-1);
-		for (int i = 0; i <= 30; ++i)		  levelCapacity.add((int) (Math.pow(2, i) - 1));
-	}
-
-	public int getLeftSubtreeSize(int nNodes) {
-		int nLeft = 0;
-		int nRight = 0;
-
-		for (int i = 1; i < levelCapacity.size(); ++i) {
-			nLeft = levelCapacity.get(i);
-			nRight = levelCapacity.get(i + 1);
-			int tmp = nNodes - 1;
-
-			if (nLeft + nRight >= tmp) {
-				for (int j = nRight; j >= nLeft; --j) {
-					for (int k = nLeft; k > levelCapacity.get(i - 1); --k) {
-						if (j + k == tmp)
-							return k;
+		// Base case: i = 0 (no keys);
+		for (int i = 0; i < ceiling; ++i) {
+			if(i == 0)
+				M[0][i] = 1;
+			else
+				M[0][i] = 0;
+		}
+		
+		// Base case h = 0 or h = 1 (empty tree or single node)
+		for(int i = 1; i < nNodes + 1; ++i){
+			M[i][0] = 0;
+			if(i == 1)
+				M[i][1] = 1;
+			else
+				M[i][1] = 0;
+		}
+		
+		// Recursive case
+		for(int i = 1; i < nNodes + 1; ++i){
+			for(int height = 0; height < ceiling; ++height){
+				if(height > 1){
+					int sum = 0;
+					for(int root = 1; root < i + 1; ++root){
+						// condition on the root and heights of left and right children
+						sum += M[root - 1][height - 1]* M[i - root][height - 1] +
+								M[root - 1][height - 2]* M[i - root][height - 1] +
+								M[root - 1][height - 1]* M[i - root][height - 2];
 					}
+					M[i][height] = sum;
 				}
 			}
 		}
-		return 0; // this should never happen~
-	}
-
-	public boolean isFullBinary(int size){
-		for(Integer i : levelCapacity){
-			if(size == i)
-				return true;
+		
+		for(int height = 0; height < ceiling; ++height){
+			numOfBBST += M[nNodes][height];
 		}
-		return false;
-	}
-	
-	public BigInteger bbstNumbers(int nNodes) {
-		BigInteger result = BigInteger.valueOf(0);
-
-		// Base cases
-		if (nNodes == 1) return BigInteger.ONE;
-		if (nNodes == 2) return BigInteger.valueOf(2);
-		if (!results[nNodes].equals(BigInteger.valueOf(-1))) {
-			return results[nNodes];
-		}
-		// Otherwise
-		int leftSize = getLeftSubtreeSize(nNodes);
-		int lower = leftSize + 1;
-		int upper = nNodes - lower + 1;
-			
-		for (int k = lower; k <= upper; ++k) {
-			int rightSize = nNodes - k;
-			if ((k - 1) > 2*rightSize && isFullBinary(k - 1)){
-				result = result.add(bbstNumbers(rightSize));
-			} else if (2*(k - 1) < rightSize && isFullBinary(rightSize)){
-				result = result.add(bbstNumbers(k - 1));
-			} else {
-				result = result.add(bbstNumbers(k - 1).multiply(bbstNumbers(nNodes - k)));
-			}
-		}
-
-		if (results[nNodes].equals(BigInteger.valueOf(-1)))
-			results[nNodes] = result;
-		return result;
+		
+		return numOfBBST;
 	}
 
 	public static void main(String[] args) {
-		int bufferSize = 200;
-		bbst run = new bbst(bufferSize);
-		System.out.println(run.bbstNumbers(15));
-		System.out.println(run.bbstNumbers(17));
-		//System.out.println(run.bbstNumbers(Integer.parseInt(args[0])));
+		bbst_sol run = new bbst_sol();
+		int nNodes = new Scanner(System.in).nextInt();
+		System.out.println(run.getBbstNum(nNodes));
 	}
 }
