7f75aa6706b3a7ee3694442290e04f90918680ad
namoshizun <ptx1962@gmail.com>
2015-10-01 11:00:17 +1000

    Update and rename bruteForce_crackDigitsPassword.java to bf_crackpw.java

--- JAVA/RandomExercises/bf_crackpw.java
+++ JAVA/RandomExercises/bf_crackpw.java
@@ -1,12 +1,9 @@
-package others;
-
 import java.util.*;
 
-public class bruteForce {
+public class bf_crackpw {
 
 	static String input;
 	static int length;
-	static boolean isFound;
 	static StringBuilder result;
 
 	public static void crack(int length, StringBuilder result) {
@@ -17,11 +14,9 @@ public class bruteForce {
 
 		for (int i = 0; i <= 9; ++i) {
 			result.setCharAt(length - 1, Character.forDigit(i, 10));
-			
 			if (result.toString().equals(input)) {
-				System.out.println("Cracked! your password is " + result);
-				isFound = true;
-				return;
+				// Very brutal way to force to stop a recursion..
+				throw new NullPointerException("Cracked! your password is " + result);
 			} else {
 				crack(length - 1, result);
 			}
@@ -34,20 +29,21 @@ public class bruteForce {
 		System.out.println("Enter a password (digits only)");
 		input = sc.nextLine();
 		length = 1;
-		isFound = false;
 
-		while (!isFound) {
-			result = new StringBuilder("");
-			for(int i = 0; i < length; ++i){
-				result.append(0);
-			}
+		try{
+			while (true) {
+				result = new StringBuilder("");
+				for(int i = 0; i < length; ++i){
+					result.append(0);
+				}
 			
-			crack(length, result);
-			++length; // increase the length of test numbers until find out the
-						// password..
+				crack(length, result);
+				++length; // increase the length of test numbers until find out the
+						// password..		
+			}		
+		} catch (NullPointerException e){
+			System.out.println(e.getMessage());
 		}
-
 		sc.close();
 	}
-
 }
